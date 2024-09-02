46da0841e1fdf7c614b10d18e3b35e0a1201e4ed
Frolov Eugene <spanditime@gmail.com>
2024-09-02 19:19:45 +0300

    dual monitor setup using autorandr

--- config/default.nix
+++ config/default.nix
@@ -35,9 +35,67 @@ in
     feh = {
       enable = true;
     };
+    # monitor config
+    autorandr = let 
+      hdmi1f="00ffffffffffff00216d00270000000023210103803c217828ee91a3544c99260f505421080031404540614081c081809500b300a9c0565e00a0a0a029503020350055502100001a000000ff0030303030303030303030303031000000fd0030901fd73c000a202020202020000000fc0048444d490a202020202020202001e102033bf14a101f0413031201403f05230907078301000067030c0010003878681a000001013090e667d85dc401788000e305c000e60605016060005b9d00a0a0a029503020350000000000001a70c200a0a0a055503020350000000000001e97e200a0a0a029503020350000000000001a000000000000000000000000000019";
+      edp1f="00ffffffffffff0006afed2100000000001c010495221378036e8593585892281e505400000001010101010101010101010101010101143780b4703826406c30aa0058c210000018b82480b4703826406c30aa0058c21000001800000000000000000000000000000000000000000002001040ff0f3c7d0f13287d2020200020";
+      eDPconf = {
+        enable = true;
+        mode = "1920x1080";
+        rotate = "normal";
+      };
+    in {
+      enable = true;
+      profiles = {
+        "single" = {
+          fingerprint={
+            eDP-1=edp1f;
+          };
+          config = {
+            eDP-1 = eDPconf // {
+              primary = true;
+            };
+          };
+        };
+        "external" = {
+          fingerprint = {
+            eDP-1=edp1f;
+            HDMI-1=hdmi1f;
+          };
+          config={
+            eDP-1.enable = false;
+            HDMI-1={
+              enable = true;
+              primary = true;
+              mode="2560x1440";
+              rate="144";
+              rotate="normal";
+            };
+          };
+        };
+        "codesetup" = {
+          fingerprint = {
+            eDP-1=edp1f;
+            HDMI-1=hdmi1f;
+          };
+          config={
+            eDP-1 = eDPconf;
+            HDMI-1={
+              enable = true;
+              primary = true;
+              mode="2560x1440";
+              rate="144";
+              rotate="right";
+              position="1980x0";
+            };
+          };
+        };
+      };
+    };
   };
   services = {
     flameshot.enable = true;
+    autorandr.enable = true;
   };
   home = {
     keyboard = {
